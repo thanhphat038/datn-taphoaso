6ec01ee8c7f2886f62151ec32d2debdedea0ede4
Thienbui <bin0931142237@gmail.com>
2025-05-30 15:50:16 +0700

    thienbui

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
+          <Route path="/login" element={<LoginPage />} />
         </Routes>
       </div>
     </Router>
