fce5fb92cbf50460aee3eacb9868b156c1f2b891
Thienbui <bin0931142237@gmail.com>
2025-05-31 19:12:04 +0700

    sua loi

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,40 +3,6 @@ import Navbar from './Navbar';
 
 const Header = () => {
     return (
-        <>
-            <header className='h-[80px] w-full border-b-1 border-[#9F9F9F]'>
-                <div className='w-[1240px] h-full m-auto flex place-content-between place-items-center gap-20'>
-
-                    <div>
-                        <img className='w-[185px]' src="./images/logo_ngang.png" alt="Logo" />
-                    </div>
-
-                    <Navbar />
-
-                    <div className='flex place-content-end place-items-center gap-10'>
-                        <div className='border-1 border-[#9F9F9F] rounded-full w-[265px] h-[40px] flex'>
-                            <input className='w-full px-4 outline-0' placeholder='Tìm kiếm...' type="text" />
-                            <button className='px-3'>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
-                                </svg>
-                            </button>
-                        </div>
-                        <div>
-                            <button className='flex place-content-end place-items-center'>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-7">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
-                                </svg>
-                            </button>
-                        </div>
-                        <div>
-                            <a href="/login" className='flex place-content-end place-items-center'>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-7">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
-                                </svg>
-                            </a>
-                        </div>
-                    </div>
         <header className='h-[80px] w-full border-b-1 border-[#9F9F9F]'>
             <div className='w-[1240px] h-full m-auto flex place-content-between place-items-center gap-20'>
 
