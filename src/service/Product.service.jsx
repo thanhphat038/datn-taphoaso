f4f25f3f891ae0938c8fdddffe9c49014f809e5e
hmdat294 <hodat2092004@gmail.com>
2025-06-01 22:10:32 +0700

    api

--- src/service/Product.service.jsx
+++ src/service/Product.service.jsx
@@ -1,11 +1,13 @@
 import axios from "axios";
 
+const api = "http://localhost:3001";
+
 export const dataProduct = () => {
-    return axios.get("http://localhost:3001/products");
+    return axios.get(`${api}/products`);
 };
 
 export const dataProductDetail = (id) => {
-    return axios.get(`http://localhost:3001/products?id=${id}`);
+    return axios.get(`${api}/products?id=${id}`);
 };
 
 // export const updatedata = (id, content) => {
