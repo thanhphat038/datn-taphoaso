f2847ddafdbe6ee0b3ff8cd9375ef00483e3539c
hmdat294 <hodat2092004@gmail.com>
2025-06-08 13:39:56 +0700

    hide header & footer for admin

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import NotFoundPage from './pages/NotFoundPage';
@@ -20,7 +20,6 @@ import ProfilePage from './pages/ProfilePage';
 
 import OrderDetailPage from './pages/OrderDetailPage';
 
-
 import ProductsPage from './pages/products';
 import ProductDetail from './pages/ProductDetail';
 import AdminUser from './pages/admin/AdminUser';
@@ -30,36 +29,43 @@ import ProductsSearch from './pages/ProductsSearch';
 
 import { CartProvider } from './context/CartContext';
 
+function Layout() {
+  const isAdminRoute = useLocation().pathname.startsWith('/admin');
+
+  return (
+    <div className="App w-full">
+      {!isAdminRoute && <Header />}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/product" element={<ProductsPage />} />
+        <Route path="/search/:value" element={<ProductsSearch />} />
+        <Route path="/product/:id" element={<ProductDetail />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/order/:id" element={<OrderDetailPage />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/select-address" element={<SelectAddress />} />
+        <Route path="/payment-success" element={<PaymentSuccess />} />
+        <Route path="*" element={<NotFoundPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/blog" element={<BlogPage />} />
+        <Route path="/blog/:id" element={<BlogDetailPage />} />
+        <Route path="/admin/user" element={<AdminUser />} />
+        <Route path="/admin/product" element={<AdminProduct />} />
+        <Route path="/admin/category" element={<AdminCategory />} />
+      </Routes>
+      {!isAdminRoute && <Footer />}
+    </div>
+  );
+}
+
 function App() {
   return (
     <Router>
       <CartProvider>
-        <div className="App w-full">
-          <Header></Header>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/product" element={<ProductsPage />} />
-            <Route path="/search/:value" element={<ProductsSearch />} />
-            <Route path="/product/:id" element={<ProductDetail />} />
-            <Route path="/about" element={<AboutPage />} />
-            <Route path="/cart" element={<CartPage />} />
-            <Route path="/profile" element={<ProfilePage />} />
-            <Route path="/order/:id" element={<OrderDetailPage />} />
-
-            <Route path="/checkout" element={<Checkout />} />
-            <Route path="/select-address" element={<SelectAddress />} />
-            <Route path="/payment-success" element={<PaymentSuccess />} />
-            <Route path="*" element={<NotFoundPage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/register" element={<RegisterPage />} />
-            <Route path="/blog" element={<BlogPage />} />
-            <Route path="/blog/:id" element={<BlogDetailPage />} />
-            <Route path="/admin/user" element={<AdminUser />} />
-            <Route path="/admin/product" element={<AdminProduct />} />
-            <Route path="/admin/category" element={<AdminCategory />} />
-          </Routes>
-          <Footer></Footer>
-        </div>
+        <Layout />
       </CartProvider>
     </Router>
   );
